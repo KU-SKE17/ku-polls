08ee0c5e6061514b219e266e89fcf7806e50c4f5
Nanthakarn Limkool <nanthakarn18@gmail.com>
2020-09-17 10:10:58 +0700

    Add a end_date attribute (part2)
    - Add a end_date attribute to Question class (polls/models.py)
    - Add end_date option to admin page

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('date closed')
 
     def __str__(self):
         return self.question_text
@@ -18,6 +19,14 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+
+    def was_closed_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.end_date <= now
+
+    was_closed_recently.admin_order_field = 'end_date'
+    was_closed_recently.boolean = True
+    was_closed_recently.short_description = 'Closed recently?'
 
 
 class Choice(models.Model):
